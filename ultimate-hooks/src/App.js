273fc909f590bfee1ad3978ced8991bf9475b429
LIWKaguya <hungle120901@gmail.com>
2021-12-01 10:51:47 +0200

    Exercise 7.8

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -1,39 +1,8 @@
-  
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React  from 'react'
+import { useField } from './hooks/useField'
+import { useResource } from './hooks/useResource'
 
 
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
-
-  // ...
-
-  const create = (resource) => {
-    // ...
-  }
-
-  const service = {
-    create
-  }
-
-  return [
-    resources, service
-  ]
-}
 
 const App = () => {
   const content = useField('text')
@@ -46,26 +15,29 @@ const App = () => {
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
+    content.reset()
   }
  
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value})
+    name.reset()
+    number.reset()
   }
 
   return (
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
+        <input type={content.type} value={content.value} onChange={content.onChange} required/>
         <button>create</button>
       </form>
       {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
+        name <input type={name.type} value={name.value} onChange={name.onChange} required/> <br/>
+        number <input type={number.type} value={number.value} onChange={number.onChange}  required/>
         <button>create</button>
       </form>
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
